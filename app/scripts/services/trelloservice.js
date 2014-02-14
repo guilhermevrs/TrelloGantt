11d546b706ab9cb306c02b17ab4db3d6aa4e3172
Guilherme Schwade <guilhermevrs@gmail.com>
2014-02-14 17:04:48 -0200

    Inicial

---
+++ app/scripts/services/trelloservice.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('trelloGanttApp')
+.service('Trelloservice', function Trelloservice() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+});
