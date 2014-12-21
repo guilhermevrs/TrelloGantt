89aeeca788ed79baa03ccc0888b174df399b3006
Guilherme Schwade <guilhermevrs@gmail.com>
2014-12-21 17:51:49 +0100

    test with spyOn

--- test/spec/services/trelloservice.js
+++ test/spec/services/trelloservice.js
@@ -7,18 +7,33 @@ describe('Service: Trelloservice', function () {
 
   // instantiate service
   var Trelloservice;
+
   beforeEach(inject(function (_Trelloservice_) {
     Trelloservice = _Trelloservice_;
   }));
-
-  it('should do something', function () {
-    expect(!!Trelloservice).toBe(true);
-  });
 
   it('should get auth token from correct local storage key', function(){
       var test_value = 'test_value';
       localStorage.setItem('trello_token', test_value);
       expect(Trelloservice.getLocalToken()).toEqual(test_value);
+  });
+
+  it('should say user is not logged when there is no trello token', function(){
+      localStorage.setItem('trello_token', null);
+      expect(Trelloservice.isUserLogged()).toBe(false);
+  });
+
+  it('should say user is logged when there is trello token', function(){
+      localStorage.setItem('trello_token', 'whatever');
+      expect(Trelloservice.isUserLogged()).toBe(true);
+  });
+
+  it('should call the correct authorize function', function(){
+      spyOn(Trello, 'authorize');
+
+      Trelloservice.authorize();
+
+      expect(Trello.authorize).toHaveBeenCalled();
   });
 
 });
