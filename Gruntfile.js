efbd055e62fd44e0cdc33f2ec99eb5a44c0a6b0d
Guilherme Schwade <guilhermevrs@gmail.com>
2015-02-03 20:16:13 +0100

    Added grunt-bump to increment versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,7 +349,26 @@ module.exports = function (grunt) {
         configFile: 'karma.conf.js',
         singleRun: true
       }
+    },
+
+    //bump configs
+    bump: {
+        options: {
+            files: ['package.json'],
+            updateConfigs: [],
+            commit: true,
+            commitMessage: 'Release v%VERSION%',
+            commitFiles: ['package.json'],
+            createTag: true,
+            tagName: 'v%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: true,
+            pushTo: 'origin',
+            gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+            globalReplace: false
+        }
     }
+
   });
 
 
